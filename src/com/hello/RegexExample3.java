69cdd48d6cc0d1c510a4e4880a4fc8fb51535ca3
Singh <manpreet.1.singh@nokia.com>
2018-12-04 14:59:11 +0530

    Innitial commit
    mkk

---
+++ src/com/hello/RegexExample3.java
@@ -0,0 +1,65 @@
+package com.hello;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+class RegexExample3
+{
+	/**
+	 *
+	 * @param args
+	 */
+	public static void main(String args[])
+	{
+		Map<Integer, String> map = new HashMap<>();
+		Map<Integer, String> updatedMap = new HashMap<>();
+		map.put(1, "P");
+		map.put(2, "N");
+		map.put(3, "O");
+		map.put(4, "M");
+
+		List<String> list = new ArrayList<>();
+		list.add("N");
+		list.add("O");
+
+		resultOut(map, updatedMap, list);
+
+	}
+
+	private static void resultOut(Map<Integer, String> map, Map<Integer, String> updatedMap, List<String> list)
+	{
+		// map.keySet().parallelStream().forEach(a -> updateData(map, updatedMap, list,
+		// a));
+
+		// System.out.println(updatedMap);
+
+		map.forEach((k, v) -> putMap(map, updatedMap, list, k, v));
+		System.out.println(updatedMap);
+	}
+
+	private static Map<Integer, String> updateData(Map<Integer, String> map, Map<Integer, String> updatedMap, List<String> list, Integer a)
+	{
+		if (list.contains(map.get(a)))
+			updatedMap.put(a,
+					map.get(a));
+
+		return updatedMap;
+	}
+
+	private static Map<Integer, String> putMap(Map<Integer, String> map, Map<Integer, String> updatedMap, List<String> list, Integer k, String v)
+	{
+		int index = abc(list, k, v);
+		if (index != -1)
+			updatedMap.put(index, map.get(index));
+		return updatedMap;
+	}
+
+	private static Integer abc(List<String> list, Integer k, String v)
+	{
+		return list.indexOf(v) != -1 ? k : -1;
+
+	}
+}
---
+++ src/com/hello/Test.java
@@ -0,0 +1,84 @@
+package com.hello;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test implements Runnable 
+{
+
+	List<String> l = Arrays.asList("N","O","K","I","A");
+    int counter = 0;
+	public  synchronized Integer getCounter() 
+	{
+		counter = counter++;
+		return counter;
+	}
+
+	public synchronized void setCounter(Integer counter) 
+	{
+		this.counter = counter;
+	}
+
+	public static void main(String[] args) 
+	{
+			Thread t1 = new Thread(new Test(),"1");
+			Thread t2 = new Thread(new Test(),"2");
+			t1.start();
+			t2.start();
+	
+	}
+
+	@Override
+	public void run() 
+	{
+		while (true)
+		{
+				printData();
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private synchronized void printData() 
+	{
+
+		//System.out.println(Thread.currentThread().getName());
+		
+		if (Thread.currentThread().getName().equals("1"))
+		{
+			counterUpdation();
+
+			System.out.print("_" + l.get(counter++));
+			notifyAll();
+			try 
+			{
+				Thread.sleep(500);
+				//l.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		else
+		{
+			counterUpdation();
+			System.out.print("_" + l.get(counter++));
+			notifyAll();
+			try {
+				Thread.sleep(500);
+				//l.wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void counterUpdation() {
+		counter = counter>4?0:counter;		
+		if(counter==0)
+			System.out.println();
+	}
+
+}
